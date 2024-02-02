9a206ce96dc41b1c75d04910bdb4808751b67068
Ohas506 <142551067+Ohas506@users.noreply.github.com>
2024-02-02 10:28:09 -0800

    -made it look retro

--- int.js
+++ int.js
@@ -1,20 +1,184 @@
 e = campaignTrail_temp;
-
-            nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#777982";
-
-            nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#6b84ff";
-
-            document.getElementsByClassName("game_header")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_title;
-
-            $("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
-
-            $(".container")[0].style.backgroundColor = "#56302f";
-
+             e.election_json = [{"model":"campaign_trail.election","pk":9,"fields":{"year":2000,"summary":"<div style='overflow-y:scroll;height:200px;'>After the unseting of George H.W Bush, the Republican party has been out of power for a whole eight years, and they smell blood in the water. W Bush son of the parties perivous leader is the favorite for the nomination, only time will tell if assumtions hold true.</div>","image_url":"","winning_electoral_vote_number":270,"advisor_url":"","recommended_reading":"<ul>\n<li><a href=https://www.chicagotribune.com/opinion/editorials/ct-edit-chicago-tribune-biden-endorsement-20200925-lnyxsb5qvrftnjjmj3rnzj33jy-story.html>Our Case For Joe Biden</a></li>\n<li><a href=https://nationalinterest.org/feature/case-trump-166808>The Case For Trump</a></li>\n<li><a href=https://www.npr.org/2020/11/18/935730100/how-biden-won-ramping-up-the-base-and-expanding-margins-in-the-suburbs>How Biden Won: Ramping Up The Base And Expanding Margins In The Suburbs</a></li>\n<li><a href=https://www.prospectmagazine.co.uk/politics/donald-trump-joe-biden-us-election-almost-win-votes-concede>Why Did Trump Almost Win?</a></li>\n<li><a href=https://www.hoover.org/research/no-evidence-voter-fraud-guide-statistical-claims-about-2020-election>No Evidence For Voter Fraud: A Guide To Statistical Claims About The 2020 Election</a></li>\n</ul>","has_visits":1,"no_electoral_majority_image":"../static/images/2012-no-majority.jpg"}}];e.temp_election_list = [{"id":9,"year":2000,"is_premium":0,"display_year":"2000"}];e.credits = "Ohas";e.global_parameter_json = [{"model":"campaign_trail.global_parameter","pk":1,"fields":{"vote_variable":1.125,"max_swing":0.12,"start_point":0.94,"candidate_issue_weight":10,"running_mate_issue_weight":3,"issue_stance_1_max":-0.71,"issue_stance_2_max":-0.3,"issue_stance_3_max":-0.125,"issue_stance_4_max":0.125,"issue_stance_5_max":0.3,"issue_stance_6_max":0.71,"global_variance":0.01,"state_variance":0.005,"question_count":25,"default_map_color_hex":"#C9C9C9","no_state_map_color_hex":"#999999"}}];e.candidate_json = [{"model":"campaign_trail.candidate","pk":79,"fields":{"first_name":"John ","last_name":"McCain","election":9,"party":"Republican","state":"Arizona","priority":1,"description":"<div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><i>\"America owes me nothing, I owe her everything. That is why I am here today, running for president\"</i><br><br>Born to a long line of Admirals, John McCain was born serving his country. McCain served his country more dutifly the most can claim, being taked a POW during the Vietnam war. His expierinces living through the tourture of his North Vietemise captiors only grew his love for America more. Returning to serve his home state of Arizona McCain has been noted as Mavrick, often not toeing the GOP line on most issues. As he runs  for president now he will have to face George W Bush, son of President HW Bush, and the presumptive favorite for the nomination. In light of this McCains campagin can be called many things, \"longshot\",\"foolish\", but to say its anything new for McCain, the life long \"dark horse\" could be farther from the truth.</div></div></div></div></div>","color_hex":"#ffd500","secondary_color_hex":null,"is_active":1,"image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/McCain2_%281%29.jpg/180px-McCain2_%281%29.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":true}},{"model":"campaign_trail.candidate","pk":77,"fields":{"first_name":"George.W","last_name":"Bush","election":9,"party":"Example Party","state":"Hell","priority":1,"description":"Dummy Example is a veteran from Hell. He loves puppies and magic! Dummy Example will unite America!<br><br>New line<br><br>This is <b>bold</b> and this <i>italic</i>!","color_hex":"#FF0000","secondary_color_hex":null,"is_active":0,"image_url":"../static/images/barack-obama-2012.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":false}},{"model":"campaign_trail.candidate","pk":77,"fields":{"first_name":"Alan","last_name":"Keyes","election":9,"party":"Example Party","state":"Hell","priority":1,"description":"Dummy Example is a veteran from Hell. He loves puppies and magic! Dummy Example will unite America!<br><br>New line<br><br>This is <b>bold</b> and this <i>italic</i>!","color_hex":"#A020F0","secondary_color_hex":null,"is_active":0,"image_url":"../static/images/barack-obama-2012.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":false}}];e.running_mate_json = [];
+            nct_stuff.themes[nct_stuff.selectedTheme].text_col = "black"
+            nct_stuff.themes[nct_stuff.selectedTheme].coloring_title = "#9959f8"; 
+            nct_stuff.themes[nct_stuff.selectedTheme].coloring_window = "#C0C0C0";
+            $(".container")[0].style.backgroundColor = "#9e9898";
+            $("#game_window")[0].style.backgroundImage = "url(https://itsastronomical.com/assets/1996_EOH/sovietbg.png)";
+            $("#game_window")[0].style.borderColor = "#C0C0C0";
             document.getElementById("header").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/McCain_2000_campaign_logo.svg/330px-McCain_2000_campaign_logo.svg.png";
 
             document.body.background = "https://www.4president.us/websites/2000/images/johnmccain2000.png";
-
-            e.election_json = [{"model":"campaign_trail.election","pk":9,"fields":{"year":2000,"summary":"<div style='overflow-y:scroll;height:200px;'>After the unseting of George H.W Bush, the Republican party has been out of power for a whole eight years, and they smell blood in the water. W Bush son of the parties perivous leader is the favorite for the nomination, only time will tell if assumtions hold true.</div>","image_url":"","winning_electoral_vote_number":270,"advisor_url":"","recommended_reading":"<ul>\n<li><a href=https://www.chicagotribune.com/opinion/editorials/ct-edit-chicago-tribune-biden-endorsement-20200925-lnyxsb5qvrftnjjmj3rnzj33jy-story.html>Our Case For Joe Biden</a></li>\n<li><a href=https://nationalinterest.org/feature/case-trump-166808>The Case For Trump</a></li>\n<li><a href=https://www.npr.org/2020/11/18/935730100/how-biden-won-ramping-up-the-base-and-expanding-margins-in-the-suburbs>How Biden Won: Ramping Up The Base And Expanding Margins In The Suburbs</a></li>\n<li><a href=https://www.prospectmagazine.co.uk/politics/donald-trump-joe-biden-us-election-almost-win-votes-concede>Why Did Trump Almost Win?</a></li>\n<li><a href=https://www.hoover.org/research/no-evidence-voter-fraud-guide-statistical-claims-about-2020-election>No Evidence For Voter Fraud: A Guide To Statistical Claims About The 2020 Election</a></li>\n</ul>","has_visits":1,"no_electoral_majority_image":"../static/images/2012-no-majority.jpg"}}];e.temp_election_list = [{"id":9,"year":2000,"is_premium":0,"display_year":"2000"}];e.credits = "Ohas";e.global_parameter_json = [{"model":"campaign_trail.global_parameter","pk":1,"fields":{"vote_variable":1.125,"max_swing":0.12,"start_point":0.94,"candidate_issue_weight":10,"running_mate_issue_weight":3,"issue_stance_1_max":-0.71,"issue_stance_2_max":-0.3,"issue_stance_3_max":-0.125,"issue_stance_4_max":0.125,"issue_stance_5_max":0.3,"issue_stance_6_max":0.71,"global_variance":0.01,"state_variance":0.005,"question_count":25,"default_map_color_hex":"#C9C9C9","no_state_map_color_hex":"#999999"}}];e.candidate_json = [{"model":"campaign_trail.candidate","pk":79,"fields":{"first_name":"John ","last_name":"McCain","election":9,"party":"Republican","state":"Arizona","priority":1,"description":"<div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><div style='overflow-y:scroll;height:200px;'><i>\"America owes me nothing, I owe her everything. That is why I am here today, running for president\"</i><br><br>Born to a long line of Admirals, John McCain was born serving his country. McCain served his country more dutifly the most can claim, being taked a POW during the Vietnam war. His expierinces living through the tourture of his North Vietemise captiors only grew his love for America more. Returning to serve his home state of Arizona McCain has been noted as Mavrick, often not toeing the GOP line on most issues. As he runs  for president now he will have to face George W Bush, son of President HW Bush, and the presumptive favorite for the nomination. In light of this McCains campagin can be called many things, \"longshot\",\"foolish\", but to say its anything new for McCain, the life long \"dark horse\" could be farther from the truth.</div></div></div></div></div>","color_hex":"#ffd500","secondary_color_hex":null,"is_active":1,"image_url":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/McCain2_%281%29.jpg/180px-McCain2_%281%29.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":true}},{"model":"campaign_trail.candidate","pk":77,"fields":{"first_name":"George.W","last_name":"Bush","election":9,"party":"Example Party","state":"Hell","priority":1,"description":"Dummy Example is a veteran from Hell. He loves puppies and magic! Dummy Example will unite America!<br><br>New line<br><br>This is <b>bold</b> and this <i>italic</i>!","color_hex":"#FF0000","secondary_color_hex":null,"is_active":0,"image_url":"../static/images/barack-obama-2012.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":false}},{"model":"campaign_trail.candidate","pk":77,"fields":{"first_name":"Alan","last_name":"Keyes","election":9,"party":"Example Party","state":"Hell","priority":1,"description":"Dummy Example is a veteran from Hell. He loves puppies and magic! Dummy Example will unite America!<br><br>New line<br><br>This is <b>bold</b> and this <i>italic</i>!","color_hex":"#A020F0","secondary_color_hex":null,"is_active":0,"image_url":"../static/images/barack-obama-2012.jpg","electoral_victory_message":"<h3>REAL PATRIOT DUMMY EXAMPLE SWEEPS</h3>","electoral_loss_message":"<h3>Fake News?</h3>","no_electoral_majority_message":"<h3>DUMMY EXAMPLE RETURNS TO HELL</h3>","description_as_running_mate":null,"candidate_score":1,"running_mate":false,"scrollable":false}}];e.running_mate_json = [];
-
-
+            $("#game_window")[0].style.backgroundColor = nct_stuff.themes[nct_stuff.selectedTheme].coloring_window;
+            
+            quotes = ["Nothing in life is more liberating than to fight for a cause larger than yourself, something that encompasses you but is not defined by your existence alone."]
+            customquote = quotes[Math.floor((Math.random() * quotes.length))]
+            corrr=`\n<h2>THE END OF THE CENTURY</h2><font id='wittyquote' size='4' color='white'><em>`+customquote+`</em></font>`
+        
+            document.head.innerHTML += '<link rel="stylesheet" href="https://unpkg.com/98.css">'
+        
+            // Select the node that will be observed for mutations
+            const windowsAdvisorTargetNode = document.getElementById("game_window");
+            
+            // Options for the observer (which mutations to observe)
+            const windowsAdvisorConfig = { attributes: true, childList: true, subtree: true };
+            
+            // Callback function to execute when mutations are observed
+            const callback = (mutationList, observer) => {
+                visitWindow = document.getElementsByClassName("overlay_window")[0];
+            
+                if(visitWindow == null || visitWindow.classList.contains("done")) {
+                    return;
+                }
+                visitWindow.classList.add("done");
+                visitWindow.classList.add("window");
+                let titleBar = visitWindow.querySelector("h3");
+                titleBar.classList.add("title-bar");
+                titleBar.classList.add("title-bar-text")
+                titleBar.style.paddingLeft = "3px";
+            };
+            
+            // Create an observer instance linked to the callback function
+            const windowsAdvisorObserver = new MutationObserver(callback);
+            
+            // Start observing the target node for configured mutations
+            windowsAdvisorObserver.observe(windowsAdvisorTargetNode, windowsAdvisorConfig);
+            
+            document.head.innerHTML += `<link
+              rel="stylesheet"
+              href="https://jetsimon.com/public/crt.css"
+            >`
+            
+            document.body.classList.add("crt");
+            
+            document.head.innerHTML += `<link
+              rel="stylesheet"
+              href="https://unpkg.com/98.css"
+            >`
+            
+            document.head.innerHTML += `<style>
+            h3 {
+                font-size:1.17em!important;
+            }
+            
+            label {
+                font-size:1em!important;
+            }
+            </style>`
+            
+            document.head.innerHTML += `<link
+              rel="stylesheet"
+              href="https://unpkg.com/98.css"
+            >`
+            
+            document.head.innerHTML += `<style>
+            h3 {
+                font-size:1.17em!important;
+            }
+            
+            label {
+                font-size:1em!important;
+                font-family: Arial, "Helvetica Neue", Helvetica, sans-serif!important;
+            }
+            </style>`
+        
+        campaignTrail_temp.modBoxTheme = {
+          "header_color": "#000080",
+          "header_text_color": "#ffffff",
+          "description_text_color": "#000000",
+          "description_background_color": "#c2c2c2",
+          "main_color": "#c2c2c2",
+          "secondary_color": "#838383",
+          "ui_text_color": "#FFFFFF"
+        }
+        
+        function addDynamicCSS() {
+          var css = `
+        
+          label {
+            display:inline-block;
+          }
+        
+          .inner_window_question h3 .mytooltip{
+              background-color: lightskyblue;
+          }
+        
+          .mytooltip {
+              position: relative;
+              display: inline-block;
+              cursor: pointer;
+              background-color: lightblue;
+          }
+        
+          .mytooltip .mytooltiptext {
+              width: 240px;
+              background-color: lightgray;
+              color: black;
+              text-align: center;
+              border-radius: 6px;
+              padding: 10px;
+              position: absolute;
+              z-index: 99;
+              bottom: 20%;
+              left: 20%;
+              margin-left: -90px;
+              opacity: 0;
+              transition: opacity 0.3s;
+              border: 1px solid black;
+              pointer-events: none;
+              font-size: 13px !important;
+              line-height: 1.5em !important;
+              font-weight: normal !important;
+              font-style: normal !important;
+          }
+        
+          .mytooltip .mytooltiptext img {
+              max-width: 240px;
+              height: auto;
+          }
+        
+          .mytooltip:hover .mytooltiptext {
+              opacity: 1;
+              transition-delay: 0.5s;
+          }
+        
+        
+        .mytooltip:hover .bubble {
+        opacity: 1;
+        transition-delay: 0.5s;
+        }
+        .bubble {
+          width: 240px;
+          background-color: lightgray;
+          color: black;
+          text-align: center;
+          border-radius: 6px;
+          padding: 10px;
+          position: absolute;
+          z-index: 99;
+          bottom: 20%;
+          left: 20%;
+          margin-left: -90px;
+          opacity: 0;
+          transition: opacity 0.3s;
+          border: 1px solid black;
+          pointer-events: none;
+          font-size: 13px !important;
+          line-height: 1.5em !important;
+          font-weight: normal !important;
+          font-style: normal !important;
+        }
+        
+        .mytooltip:hover .bubble-bottom-left:before,
+        .mytooltip:hover .bubble-bottom-left:after {
+        opacity: 1;
+        }
+          `;
+        
+          var styleElem = document.createElement('style');
+          styleElem.type = 'text/css';
           
+          styleElem.appendChild(document.createTextNode(css));
+          
+          document.head.appendChild(styleElem);
+        }
